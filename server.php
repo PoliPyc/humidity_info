e9f46d6581ff377d5441be70879fd683aa0d3911
Jakub Czerwiński <poliakustyczny@gmail.com>
2020-12-17 13:51:31 +0100

    use fontawesome instead of glyphicons

--- server.php
+++ server.php
@@ -11,9 +11,9 @@ function getLastLines($string, $n = 1) {
 
 function set_trend_icon($trend) {
 	if($trend > 1) {
-		return '<i class="glyphicon glyphicon-arrow-up" />';
+		return '<i class="fas fa-long-arrow-alt-up text-success"></i>';
 	} elseif($trend < 1) {
-		return '<i class="glyphicon glyphicon-arrow-down" />';
+		return '<i class="fas fa-long-arrow-alt-down text-danger"></i>';
 	}
 }
 
@@ -24,6 +24,7 @@ foreach($lines as $row){
 	$row_data = explode(',', $row);
 	$row_data[6] = set_trend_icon($row_data[4]);
 	$row_data[7] = set_trend_icon($row_data[5]);
+
 	$data[] = $row_data;
 }
 ?>
@@ -34,20 +35,21 @@ foreach($lines as $row){
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/7d9a5e777c.js" crossorigin="anonymous"></script>
 </head>
 <body class="bg-dark">
 <div class="container">
 	<div class="row">
 		<h1>DHT-22</h1>
 	</div>
-	<div class="row">
+	<div class="row"><span class="glyphicon glyphicon-arrow-up"></span>
 		<table class="table table-striped table-bordered table-hover table-dark">
 		<thead class="bg-info">
 			<tr>
-				<td class="col-3">Data</td>
-				<td class="col-3">Czas</td>
-				<td class="col-3">Temp.</td>
-				<td class="col-3">Wilgotność</td>
+				<td class="">Data</td>
+				<td class="">Czas</td>
+				<td class="">Temp.</td>
+				<td class="">Wilgotność</td>
 		</thead>
 		<tbody>
 			<?php foreach($data as $row):?>
